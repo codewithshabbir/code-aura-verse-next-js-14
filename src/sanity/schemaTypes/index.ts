0069366e7377855be8469b56ba96e2b5c69ad463
Muhammad Shabbir <110288732+muhammadshabbir07@users.noreply.github.com>
2025-01-07 05:15:28 +0500

    sanity add

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import blog from '../blogPost'
+import author from '../author'
+import category from '../category'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [blog, author, category],
 }
